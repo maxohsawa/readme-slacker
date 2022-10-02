349bab46accab33900b1351e29d2790a1048660f
Max Ohsawa <max.ohsawa@gmail.com>
2022-10-01 21:13:47 -0400

    handles bolding headers, acceptance criteria emojis, expected behavior emojis, comment emoji, and bonus emoji

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
-// node CLI: https://developer.okta.com/blog/2019/06/18/command-line-app-with-nodejs
+// node CLI:
+// https://developer.okta.com/blog/2019/06/18/command-line-app-with-nodejs
 
-import { readData, writeData, addTitle } from '../helpers.js';
+import {
+  readData,
+  writeData,
+  addTitle,
+  removeEmojis,
+  modifyByLine,
+} from '../helpers.js';
 
 console.log('=== README SLACKER ===');
 console.log('.. lazily generating a slackable post out of an activity README');
@@ -10,5 +17,6 @@ console.log('.. lazily generating a slackable post out of an activity README');
 let data = await readData();
 
 data = addTitle(data);
-
+data = removeEmojis(data);
+data = modifyByLine(data);
 writeData(data);
--- helpers.js
+++ helpers.js
@@ -1,6 +1,20 @@
 import fs from 'fs';
 import path, { basename } from 'path';
 
+const CLASS_EMOJIS = [
+  ':shield:',
+  ':hammer:',
+  ':gear:',
+  ':snake:',
+  ':glove:',
+  ':crystal_ball:',
+  ':staff_of_aesculapius',
+  ':bow_and_arrow:',
+  ':ant:',
+  ':eagle:',
+  ':mechanical_arm:',
+];
+
 export async function readData() {
   try {
     let data = await fs.promises.readFile('README.md');
@@ -20,4 +34,74 @@ export async function writeData(data) {
 
 export function addTitle(data) {
   return `*= ${path.basename(process.cwd())} =*\n\n${data}`;
+}
+
+// emoji regex:
+// https://www.freecodecamp.org/news/how-to-use-regex-to-match-emoji-including-discord-emotes/
+export function removeEmojis(data) {
+  const emojiRegex = /\s<a?:.+?:\d{18}>|\s\p{Extended_Pictographic}/gu;
+  return data.replace(emojiRegex, '');
+}
+
+export function modifyByLine(data) {
+  // iterate through lines
+  const lines = data.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    // if the line is '## Acceptance Criteria' then replace following '* ' with class emojis
+    if (lines[i] === '## Acceptance Criteria') {
+      let emojiNo = 0;
+
+      for (let j = i + 1; j < lines.length; j++) {
+        if (lines[j][0] === '*') {
+          lines[j] = CLASS_EMOJIS[emojiNo++] + lines[j].substring(1);
+        }
+
+        // break when next section is reached
+        if (lines[j][0] === '#') {
+          break;
+        }
+      }
+    }
+
+    // if the line is '## Expected Behavior' then prepend class emojis to following lines
+    if (lines[i] === '## Expected Behavior') {
+      let emojiNo = 0;
+
+      for (let j = i + 1; j < lines.length; j++) {
+        if (lines[j][0] > 'A' && lines[j][0] < 'Z') {
+          lines[j] = CLASS_EMOJIS[emojiNo++] + ' ' + lines[j].substring(0);
+        }
+
+        // break when next section is reached
+        if (lines[j][0] === '#') {
+          break;
+        }
+      }
+    }
+
+    // if the line starts with 'Work with a partner to add comments' then prepend line with class emoji
+    if (lines[i].match(/Work with a partner to add comments/)) {
+      lines[i] = CLASS_EMOJIS[0] + ' ' + lines[i].substring(0);
+    }
+
+    // if the line is '## Bonus' then replace the following '* ' with ':zap:'
+    if (lines[i] === '## Bonus') {
+      for (let j = i + 1; j < lines.length; j++) {
+        if (lines[j][0] === '*') {
+          lines[j] = ':zap:' + lines[j].substring(1);
+
+          break;
+        }
+      }
+    }
+
+    // if header remove hashtag(s) and enclose line in asterisks
+    if (lines[i].match(/#️?/)) {
+      lines[i] = lines[i].trim();
+      const words = lines[i].split(' ');
+      lines[i] = '*' + words.slice(1).join(' ') + '*';
+    }
+  }
+
+  return lines.join('\n');
 }
