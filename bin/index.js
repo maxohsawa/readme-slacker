75fc18ab9cc103e6ebb2e27509a3ceb8863f173a
Max Ohsawa <max.ohsawa@gmail.com>
2022-10-04 14:44:12 -0400

    adds OS check: macOS defaults to clipboard, windows defaults to text file; adds '-f' option to select text file feature

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 // node CLI:
 // https://developer.okta.com/blog/2019/06/18/command-line-app-with-nodejs
 
+import proc from 'child_process';
+
 import {
   readData,
   writeData,
@@ -20,4 +22,17 @@ data = addTitle(data);
 data = removeEmojis(data);
 data = modifyByLine(data);
 
-writeData(data);
+if (
+  process.platform === 'win32' ||
+  (process.argv.length === 3 && process.argv[2] === '-f')
+) {
+  console.log('.. writing file');
+  writeData(data);
+} else if (process.platform === 'darwin') {
+  console.log('.. default macOS behavior');
+  console.log('.. copying to clipboard');
+  // https://stackoverflow.com/questions/7778539/copy-to-clipboard-in-node-js
+  const proc_pbcopy = proc.spawn('pbcopy');
+  proc_pbcopy.stdin.write(data);
+  proc_pbcopy.stdin.end();
+}
